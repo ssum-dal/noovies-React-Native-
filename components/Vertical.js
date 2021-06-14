aa61178c4c5b447591b7326a29e5321be3073fd1
ssum-dal <min8226@gmail.com>
2021-06-14 17:56:15 +0900

    style 수정, trimText 추가

--- components/Vertical.js
+++ components/Vertical.js
@@ -1,25 +1,34 @@
 import React from "react";
 import styled from "styled-components/native";
 import PropTypes from "prop-types";
+import {TouchableOpacity} from "react-native";
+import { trimText } from "../utils";
 import Poster from "./Poster";
-import { apiImage } from "../api";
 import Votes from "./Votes";
 
-const Container = styled.View``;
+const Container = styled.View`
+  align-items: center;
+  margin-right: 20px;
+`;
 
 const Title = styled.Text`
   color: white;
+  font-weight: 500;
+  margin: 10px 0px 5px 0px;
 `;
 
-const Vertical = ({ poster, title, votes }) => (
-  <Container>
-    <Poster url={apiImage(poster)} />
-    <Title>{title}</Title>
-    <Votes votes={votes} />
-  </Container>
+const Vertical = ({id, poster, title, votes }) => (
+  <TouchableOpacity>
+    <Container>
+      <Poster url={poster} />
+      <Title>{trimText(title, 10)}</Title>
+      <Votes votes={votes} />
+    </Container>
+  </TouchableOpacity>
 );
 
 Vertical.propTypes = {
+  id: PropTypes.number.isRequired,
   poster: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   votes: PropTypes.number.isRequired
